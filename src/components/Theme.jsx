d22aead746c5d67be6a1bb8861a368b7e059465c
Yiting Lin <etlin1401@gmail.com>
2022-06-02 11:51:29 +0800

    Add files via upload

---
+++ src/components/Theme.jsx
@@ -0,0 +1,36 @@
+import React, { useMemo, useEffect } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo'
+
+function Theme({ dark, setDark }){
+
+    const themeStyle = useMemo(() => {
+      return {
+        backgroundColor: dark ? '#2c3e50': '#ADD8E6',
+        color: dark ? '#ecf0f1' : '#234363'
+      }
+    }, [dark])
+  
+    useEffect(() => {
+      bodytheme();
+    }, [themeStyle])
+  
+    const changeTheme = () => {
+      setDark(prevDark => !prevDark)
+    }
+  
+    const bodytheme = (() => {
+      document.body.style.backgroundColor = themeStyle.backgroundColor;
+      document.body.style.color = themeStyle.color;
+    })
+
+    return(
+        <div id='button' onClick={changeTheme}>
+        <StyledEngineProvider injectFirst class="center start" >
+          <Demo/>
+        </StyledEngineProvider>
+      </div>
+    )
+}
+
+export default Theme;
